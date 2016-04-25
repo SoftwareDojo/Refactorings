c0228906ee4b48b939f8af9f649743348bc07a9a
SoftwareDojo <nextexception@gmail.com>
2016-04-25 10:22:37 +0200

    Added a refactored solution for Yatzy and Tests

---
+++ C#/CSharpRefactoringTests/Yatzy/Refactored/YatzyScorerTest.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using CSharpRefactorings.Yatzy.Refactored;
+using NUnit.Framework;
+
+namespace CSharpRefactoringTests.Yatzy.Refactored
+{
+    [TestFixture]
+    public class YatzyScorerTest
+    {
+        [TestCase(new[] { 2, 3, 4, 5, 1 }, 15)]
+        [TestCase(new[] { 3, 3, 4, 5, 1 }, 16)]
+        public void Chance_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.Chance);
+        }
+
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, 1)]
+        [TestCase(new[] { 1, 2, 1, 4, 5 }, 2)]
+        [TestCase(new[] { 6, 2, 2, 4, 5 }, 0)]
+        [TestCase(new[] { 1, 2, 1, 1, 1 }, 4)]
+        public void Ones_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.Ones);
+        }
+
+        [TestCase(new[] { 1, 2, 3, 2, 6 }, 4)]
+        [TestCase(new[] { 2, 2, 2, 2, 2 }, 10)]
+        public void Twos_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.Twos);
+        }
+
+        [TestCase(new[] { 1, 2, 3, 2, 3 }, 6)]
+        [TestCase(new[] { 2, 3, 3, 3, 3 }, 12)]
+        public void Threes_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.Threes);
+        }
+
+        [TestCase(new[] { 4, 5, 5, 5, 5 }, 4)]
+        [TestCase(new[] { 4, 4, 5, 5, 5 }, 8)]
+        [TestCase(new[] { 4, 4, 4, 5, 5 }, 12)]
+        public void Fours_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.Fours);
+        }
+
+        [TestCase(new[] { 4, 4, 4, 5, 5 }, 10)]
+        [TestCase(new[] { 4, 4, 5, 5, 5 }, 15)]
+        [TestCase(new[] { 4, 5, 5, 5, 5 }, 20)]
+        public void Fives_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.Fives);
+        }
+
+        [TestCase(new[] { 4, 4, 4, 5, 5 }, 0)]
+        [TestCase(new[] { 4, 4, 6, 5, 5 }, 6)]
+        [TestCase(new[] { 6, 5, 6, 6, 5 }, 18)]
+        public void Sixes_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.Sixes);
+        }
+
+        [TestCase(new[] { 6, 6, 6, 6, 3 }, 0)]
+        [TestCase(new[] { 4, 4, 4, 4, 4 }, 50)]
+        [TestCase(new[] { 6, 6, 6, 6, 6 }, 50)]
+        public void Yatzy_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.Yatzy);
+        }
+
+        [TestCase(new[] { 3, 4, 2, 5, 6 }, 0)]
+        [TestCase(new[] { 3, 4, 3, 5, 6 }, 6)]
+        [TestCase(new[] { 5, 3, 3, 3, 5 }, 10)]
+        [TestCase(new[] { 5, 3, 6, 6, 5 }, 12)]
+        [TestCase(new[] { 6, 6, 6, 6, 5 }, 12)]
+        public void OnePair_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.OnePair);
+        }
+
+        [TestCase(new[] { 3, 4, 2, 5, 5 }, 0)]
+        [TestCase(new[] { 3, 3, 5, 5, 5 }, 16)]
+        [TestCase(new[] { 3, 3, 5, 4, 5 }, 16)]
+        public void TwoPair_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.TwoPair);
+        }
+
+        [TestCase(new[] { 3, 3, 3, 4, 5 }, 9)]
+        [TestCase(new[] { 3, 3, 3, 3, 5 }, 9)]
+        [TestCase(new[] { 5, 3, 5, 4, 5 }, 15)]
+        public void ThreeOfAKind_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.ThreeOfAKind);
+        }
+
+        [TestCase(new[] { 3, 3, 3, 3, 5 }, 12)]
+        [TestCase(new[] { 3, 3, 3, 3, 3 }, 12)]
+        [TestCase(new[] { 5, 5, 5, 4, 5 }, 20)]
+        public void FourOfAKind_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.FourOfAKind);
+        }
+
+        [TestCase(new[] { 1, 2, 2, 4, 5 }, 0)]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, 15)]
+        [TestCase(new[] { 2, 3, 4, 5, 1 }, 15)]
+        public void SmallStraight_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.SmallStraight);
+        }
+
+        [TestCase(new[] { 1, 2, 2, 4, 5 }, 0)]
+        [TestCase(new[] { 2, 3, 4, 5, 6 }, 20)]
+        [TestCase(new[] { 6, 2, 3, 4, 5 }, 20)]
+        public void LargeStraight_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.LargeStraight);
+        }
+
+        [TestCase(new[] { 2, 3, 4, 5, 6 }, 0)]
+        [TestCase(new[] { 5, 5, 5, 5, 6 }, 0)]
+        [TestCase(new[] { 6, 2, 2, 2, 6 }, 18)]
+        public void FullHouse_score(int[] dice, int expected)
+        {
+            AreEqual(dice, expected, YatzyScorer.FullHouse);
+        }
+
+        private void AreEqual(int[] dice, int expected, Func<int[], int> scoreAction)
+        {
+            // act
+            int actual = scoreAction(dice);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ C#/CSharpRefactorings/Yatzy/Refactored/YatzyScorer.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpRefactorings.Yatzy.Refactored
+{
+    public static class YatzyScorer
+    {
+        private const int s_MinValue = 1;
+        private const int s_MaxValue = 6;
+        private const int s_DiceCount = 5;
+
+        public static int Chance(int[] dice)
+        {
+            CheckDice(dice);
+            return dice.Sum();
+        }
+
+        public static int Ones(int[] dice)
+        {
+            CheckDice(dice);
+            return DiceCount(dice, 1);
+        }
+
+        public static int Twos(int[] dice)
+        {
+            CheckDice(dice);
+            return DiceCount(dice, 2) * 2;
+        }
+
+        public static int Threes(int[] dice)
+        {
+            CheckDice(dice);
+            return DiceCount(dice, 3) * 3;
+        }
+
+        public static int Fours(int[] dice)
+        {
+            CheckDice(dice);
+            return DiceCount(dice, 4) * 4;
+        }
+
+        public static int Fives(int[] dice)
+        {
+            CheckDice(dice);
+            return DiceCount(dice, 5) * 5;
+        }
+
+        public static int Sixes(int[] dice)
+        {
+            CheckDice(dice);
+            return DiceCount(dice, 6) * 6;
+        }
+
+        public static int Yatzy(int[] dice)
+        {
+            CheckDice(dice);
+            return MaxDiceCount(dice, s_DiceCount) > 0 ? 50 : 0;
+        }
+
+        public static int OnePair(int[] dice)
+        {
+            CheckDice(dice);
+            return 2*MaxDiceCount(dice, 2);
+        }
+
+        public static int TwoPair(int[] dice)
+        {
+            CheckDice(dice);
+
+            var sum = 0;
+            for (int i = s_MaxValue; i >= s_MinValue; i--)
+            {
+                if (DiceCount(dice, i) < 2) continue;
+                if (sum == 0) sum = 2*i;
+                else return sum + 2*i;
+            }
+
+            return 0;
+        }
+
+        public static int ThreeOfAKind(int[] dice)
+        {
+            CheckDice(dice);
+            return MaxDiceCount(dice, 3) * 3;
+        }
+
+        public static int FourOfAKind(int[] dice)
+        {
+            CheckDice(dice);
+            return MaxDiceCount(dice, 4) * 4;
+        }
+
+        public static int SmallStraight(int[] dice)
+        {
+            CheckDice(dice);
+
+            for (int i = s_MinValue; i < s_MaxValue; i++)
+            {
+                if (DiceCount(dice, i) != 1) return 0;
+            }
+
+            return 15;
+        }
+
+        public static int LargeStraight(int[] dice)
+        {
+            CheckDice(dice);
+
+            for (int i = s_MaxValue; i > s_MinValue; i--)
+            {
+                if (DiceCount(dice, i) != 1) return 0;
+            }
+
+            return 20;
+        }
+
+        public static int FullHouse(int[] dice)
+        {
+            CheckDice(dice);
+
+            var threeOfAKind = MaxDiceCount(dice, 3);
+            if (threeOfAKind == 0) return 0;
+
+            for (int i = s_MaxValue; i >= s_MinValue; i--)
+            {
+                if (i == threeOfAKind) continue;
+                if (DiceCount(dice, i) == 2) return (2*i) + (3*threeOfAKind);
+            }
+
+            return 0;
+        }
+
+        private static int MaxDiceCount(int[] dice, int minCount)
+        {
+            for (int i = s_MaxValue; i >= s_MinValue; i--)
+            {
+                if (DiceCount(dice, i) >= minCount) return i;
+            }
+
+            return 0;
+        }
+
+        private static int DiceCount(int[] dice, int condition)
+        {
+            return dice.Count(d => d == condition);
+        }
+
+        private static void CheckDice(int[] dice)
+        {
+            if (dice == null) throw new ArgumentNullException(nameof(dice));
+            if (dice.Length != s_DiceCount) throw new ArgumentException(nameof(dice));
+
+            foreach (var value in dice)
+            {
+                if (value < s_MinValue || value > s_MaxValue) throw new ArgumentException(nameof(dice));
+            }
+        }
+    }
+}
